4ee37d367bebe3ecf9b80be5d326107e37a62a15
卢小玉 <923121067@qq.com>
2016-11-10 08:40:36 +0800

    昨天的的代码, 实现了食物百科中Item的点击事件, 第一次进入的数据成功显示, 但是营养素的弹窗中的 Item 的颜色变换和数据的变换还未实现.

---
+++ app/src/main/java/com/example/dllo/food/library/more/MyLibraryMoreLvAdapter.java
@@ -0,0 +1,7 @@
+package com.example.dllo.food.library.more;
+
+/**
+ * Created by dllo on 16/11/8.
+ */
+public class MyLibraryMoreLvAdapter {
+}
---
+++ app/src/main/java/com/example/dllo/food/tools/OnRecyclerViewItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.dllo.food.tools;
+
+/**
+ * Created by dllo on 16/11/9.
+ */
+public interface OnRecyclerViewItemClickListener {
+}
